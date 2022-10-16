58f48b2b7a794bc5d3ecf55f72d49db73f6ecaf4
Vinicius Basilio <vbasilio2019@gmail.com>
2022-10-16 11:59:37 -0300

    atualizacao CRUD

--- Advocacia/src/VIEW/CRUD.java
+++ Advocacia/src/VIEW/CRUD.java
@@ -19,14 +19,6 @@ public class CRUD extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         crudCadastrar = new javax.swing.JMenu();
         crudCadastrarFuncionarios = new javax.swing.JMenuItem();
-        crudCadastrarProcesso = new javax.swing.JMenuItem();
-        crudAlterar = new javax.swing.JMenu();
-        crudAlterarFuncionarios = new javax.swing.JMenuItem();
-        crudConsultar = new javax.swing.JMenu();
-        crudListaFuncionarios = new javax.swing.JMenuItem();
-        crudListagemProcessos = new javax.swing.JMenuItem();
-        crudExcluir = new javax.swing.JMenu();
-        crudExcluirFuncionarios = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -55,54 +47,7 @@ public class CRUD extends javax.swing.JFrame {
         });
         crudCadastrar.add(crudCadastrarFuncionarios);
 
-        crudCadastrarProcesso.setText("Cadastrar Processo");
-        crudCadastrarProcesso.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                crudCadastrarProcessoActionPerformed(evt);
-            }
-        });
-        crudCadastrar.add(crudCadastrarProcesso);
-
         jMenuBar1.add(crudCadastrar);
-
-        crudAlterar.setText("Alterar");
-
-        crudAlterarFuncionarios.setText("Alterar Funcionários");
-        crudAlterarFuncionarios.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                crudAlterarFuncionariosActionPerformed(evt);
-            }
-        });
-        crudAlterar.add(crudAlterarFuncionarios);
-
-        jMenuBar1.add(crudAlterar);
-
-        crudConsultar.setText("Consultar");
-
-        crudListaFuncionarios.setText("Consultar Funcionários");
-        crudListaFuncionarios.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                crudListaFuncionariosActionPerformed(evt);
-            }
-        });
-        crudConsultar.add(crudListaFuncionarios);
-
-        crudListagemProcessos.setText("Listagem de Processos");
-        crudListagemProcessos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                crudListagemProcessosActionPerformed(evt);
-            }
-        });
-        crudConsultar.add(crudListagemProcessos);
-
-        jMenuBar1.add(crudConsultar);
-
-        crudExcluir.setText("Excluir");
-
-        crudExcluirFuncionarios.setText("Excluir Funcionário");
-        crudExcluir.add(crudExcluirFuncionarios);
-
-        jMenuBar1.add(crudExcluir);
 
         setJMenuBar(jMenuBar1);
 
@@ -130,36 +75,10 @@ public class CRUD extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_crudCadastrarFuncionariosActionPerformed
 
-    private void crudListaFuncionariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudListaFuncionariosActionPerformed
-        new Consultar().setVisible(true);
-        dispose();
-    }//GEN-LAST:event_crudListaFuncionariosActionPerformed
-
-    private void crudCadastrarProcessoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudCadastrarProcessoActionPerformed
-        new CadastrarProcesso().setVisible(true);
-        dispose();
-    }//GEN-LAST:event_crudCadastrarProcessoActionPerformed
-
-    private void crudListagemProcessosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudListagemProcessosActionPerformed
-
-    }//GEN-LAST:event_crudListagemProcessosActionPerformed
-
-    private void crudAlterarFuncionariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudAlterarFuncionariosActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_crudAlterarFuncionariosActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu crudAlterar;
-    private javax.swing.JMenuItem crudAlterarFuncionarios;
     private javax.swing.JMenu crudCadastrar;
     private javax.swing.JMenuItem crudCadastrarFuncionarios;
-    private javax.swing.JMenuItem crudCadastrarProcesso;
-    private javax.swing.JMenu crudConsultar;
-    private javax.swing.JMenu crudExcluir;
-    private javax.swing.JMenuItem crudExcluirFuncionarios;
-    private javax.swing.JMenuItem crudListaFuncionarios;
-    private javax.swing.JMenuItem crudListagemProcessos;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
