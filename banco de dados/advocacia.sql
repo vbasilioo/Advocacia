533eeb55740b70884878bbd8bed35c6bfa901e4d
Vinicius Basilio <vbasilio2019@gmail.com>
2022-10-26 12:33:33 -0300

    janela de administrador e usuarios

--- banco de dados/advocacia.sql
+++ banco de dados/advocacia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Out-2022 às 15:40
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.0.13
+-- Tempo de geração: 26-Out-2022 às 17:32
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,20 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `cargos`
+--
+
+CREATE TABLE `cargos` (
+  `id_cargo` int(11) NOT NULL,
+  `descricao_cargo` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cargos`
+--
+
+INSERT INTO `cargos` (`id_cargo`, `descricao_cargo`) VALUES
+(1, 'Administrador'),
+(2, 'Advogado');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `nome_usuario` varchar(100) NOT NULL,
-  `senha_usuario` varchar(100) NOT NULL,
-  `cargo_usuario` int(11) NOT NULL,
-  `id_processo.associado` int(11) NOT NULL
+  `nome_usuario` varchar(50) NOT NULL,
+  `senha_usuario` varchar(30) NOT NULL,
+  `email_usuario` varchar(50) NOT NULL,
+  `cargo_usuario` varchar(25) NOT NULL,
+  `id_processo_associado` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `senha_usuario`, `email_usuario`, `cargo_usuario`, `id_processo_associado`) VALUES
+(1, 'admin', 'admin', 'admin@admin.com', 'Administrador', '0'),
+(2, 'vinicius', '1234', 'vinicius@usuario.com', 'Advogado', '123');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `cargos`
+--
+ALTER TABLE `cargos`
+  ADD PRIMARY KEY (`id_cargo`);
 
 --
 -- Índices para tabela `usuarios`
@@ -50,10 +84,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `cargos`
+--
+ALTER TABLE `cargos`
+  MODIFY `id_cargo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
