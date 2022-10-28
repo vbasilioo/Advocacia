4b894c5e8bd9fa58f9f75a7c30d046a6f074f528
Vinícius Basílio <99216154+vbasilioo@users.noreply.github.com>
2022-10-28 10:36:59 -0300

    Delete logger.slf4j-spring-master directory

--- logger.slf4j-spring-master/src/main/java/br/com/apiexemplo/repositories/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.apiexemplo.repositories;
-
-import br.com.apiexemplo.models.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
-}
--- logger.slf4j-spring-master/src/main/java/br/com/apiexemplo/repositories/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.apiexemplo.repositories;
-
-import br.com.apiexemplo.models.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
