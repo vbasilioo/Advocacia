1e0fdbf54b4aef707fbb641c4af1387b42d22b6f
samlopes <samrslopes@gmail.com>
2022-11-21 19:55:49 -0300

    Correção erro lista de usuarios. @srslopes

--- backend/Advocacia/src/VIEW/EditarProcesso.java
+++ backend/Advocacia/src/VIEW/EditarProcesso.java
@@ -34,7 +34,10 @@ public class EditarProcesso extends javax.swing.JFrame {
     
     public EditarProcesso() {
         
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+           public void run() {                
+            }
+        });
             
         
         text=ConsultarProcesso.usuarios;
@@ -324,37 +327,7 @@ public class EditarProcesso extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EditarProcesso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EditarProcesso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EditarProcesso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EditarProcesso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-           public void run() {
-                new EditarProcesso().setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonAdicionar;
